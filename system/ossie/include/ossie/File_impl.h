4407662b4a12481ab7de8a71a8d1a3020e9d8693
ClausKlein <claus.klein@arcormail.de>
2021-12-07 21:24:28 +0100

    prevent a warning about include guard

--- system/ossie/include/ossie/File_impl.h
+++ system/ossie/include/ossie/File_impl.h
@@ -22,7 +22,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ****************************************************************************/
 
 /* SCA */
-#ifndef FILE_IMPL__H
+#ifndef FILE_IMPL_H
 #define FILE_IMPL_H
 
 /* Include files */
